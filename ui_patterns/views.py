cf5f88863b74902d69f75889a7566bc589974b8b
ericlindstrom <eric.lindstrom@gmail.com>
2013-05-03 14:14:37 -0700

    ordering update

--- ui_patterns/views.py
+++ ui_patterns/views.py
@@ -2,12 +2,12 @@ from django.views.generic import TemplateView, ListView, DetailView
 from .models import *
 
 class IndexView(ListView):
-    queryset = Pattern.objects.live()
+    queryset = Pattern.objects.live().order_by('order')
 
 class PatternCategoryView(ListView):
     def get_queryset(self):
         return Pattern.objects.filter(category__slug=self.kwargs['category'],
-          category__visible=True)
+          category__visible=True).order_by('order')
 
 
 class PatternDetailView(DetailView):
